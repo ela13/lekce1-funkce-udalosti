23edcdbd0fc662e8ecfc1d86451f161651fa1e49
LAPTOP-8PC2QIAJ\Nin-ki <ela.nekardova@gmail.com>
2021-03-13 16:11:34 +0100

    Ahoj, odevzdávám úkol. Snad ok. :) Ela

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+/**
+ * Upozorní uživatele při spuštění.
+ */
+
+function upozorni() {
+	alert('Gratulace, právě jsi spustila tuto funkci!');
+	console.log('Gratulace, právě jsi spustila tuto funkci!');
+
+	let squareElement = document.getElementsByClassName('ctverecek')[0]
+	squareElement.innerHTML = 'Gratulace, právě jsi spustila tuto funkci!';
+}
+
+/** a change of the square background */
+function zmenBarvu(elementSelector) {
+	document.querySelector(elementSelector).style.backgroundColor = 'green';
+}
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -8,14 +23,15 @@ document.querySelector("#vysledek").innerHTML = secti(4, 5);
  * @param {int} a
  * @param {int} b
  */
+
+ function zobrazVysledek(a, b) {
+	document.querySelector('#vysledek').innerHTML = secti(a, b);
+}
+
 function secti(a, b) {
   let c = a + b;
   return c;
 }
 
-/**
- * Upozorní uživatele při spuštění.
- */
-function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
-}
+
+
